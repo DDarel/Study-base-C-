692439072e5405c4298bc169d2b14cb6d525de97
Vladislav <darel.dahanavar.shevchyk@gmail.com>
2019-03-25 23:49:36 +0300

    Lesson 2 Homework 3

---
+++ Lesson 2 Homework 3/Lesson 2 Homework 3.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+	setlocale(0, "");
+	cout << "Введите последовательность (-1 для завершения)\n";
+
+	int sum = 0;
+	int count = -1;
+	int N = 0;
+	float average;
+
+	while (N != -1)
+	{
+		sum += N;
+		cin >> N;
+		count++;
+	}
+
+	average = float(sum) / float(count);
+
+
+	cout << "\nСумма  - " << sum << endl;
+	cout << "\nСреднее арифметическое - " << average << endl;
+	return 0;
+
+}
