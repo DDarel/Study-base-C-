e627ef1548b85f6aabd9244e7603162c13e48e83
Vladislav <darel.dahanavar.shevchyk@gmail.com>
2019-03-26 00:03:05 +0300

    Счастливый билет

---
+++ Lesson 3 Homework 1/Lesson 3 Homework 1.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	char builet[7];
+
+	cout << "Введите число:";
+	fgets(builet, sizeof(builet), stdin);
+
+	cout << endl;
+	cout << "Первое число:" << builet[0] << endl;
+	cout << "Второе число:" << builet[1] << endl;
+	cout << "Третье число:" << builet[2] << endl;
+	cout << "Четвертое число:" << builet[3] << endl;
+	cout << "Пятое число:" << builet[4] << endl;
+	cout << "Шестое число:" << builet[5] << endl;
+	cout << endl;
+
+
+	if (builet[0] + builet[1] + builet[2] == builet[3] + builet[4] + builet[5])
+	{
+		cout << builet << " - суммы совпадают - билет счастливый" << endl;
+	}
+	else
+	{
+		cout << builet << "- суммы не совпадают - билет не счастливый" << endl;
+
+	}
+	return 0;
+}
